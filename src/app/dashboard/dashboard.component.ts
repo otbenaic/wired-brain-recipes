0f71fe986e62785679d2116077b975e4520029a4
Benaich Otmane <benaich.otmane@gmail.com>
2020-04-29 21:43:52 +0000

    first commit

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+
+import { Book } from 'app/models/book';
+import { Reader } from 'app/models/reader';
+import { DataService } from 'app/core/data.service';
+import { createEmptyStateSnapshot } from '@angular/router/src/router_state';
+import { BookTrackerError } from 'app/models/bookTrackerError';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styles: []
+})
+export class DashboardComponent implements OnInit {
+
+  allBooks: Book[];
+  allReaders: Reader[];
+  mostPopularBook: Book;
+
+  constructor(private dataService: DataService,
+              private title: Title,
+              private router: ActivatedRoute) { }
+
+  ngOnInit() {
+    const resolvedData: Book[] | BookTrackerError = this.router.snapshot.data['resolvedBooks'];
+
+    if (resolvedData instanceof BookTrackerError) {
+      console.log(`Dashboard component error: ${resolvedData.friendlyMessage}`);
+    } else {
+      this.allBooks = resolvedData;
+    }
+    this.allReaders = this.dataService.getAllReaders();
+    this.mostPopularBook = this.dataService.mostPopularBook;
+
+    this.title.setTitle(`Book Tracker`);
+  }
+
+  deleteBook(bookID: number): void {
+    this.dataService.deleteBook(bookID)
+    .subscribe(
+      (data: void) => {
+        const index: number = this.allBooks.findIndex(book => book.bookID === bookID);
+        this.allBooks.splice(index, 1);
+      },
+      (err: any) => console.log(err)
+    );
+  }
+
+  deleteReader(readerID: number): void {
+    console.warn(`Delete reader not yet implemented (readerID: ${readerID}).`);
+  }
+
+}
